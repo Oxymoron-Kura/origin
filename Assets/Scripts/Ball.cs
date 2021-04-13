09d8a6c288a66900e3cfc7de7cb7c51919192df7
DESKTOP-VRIF84H\Yagi Yusuke <yagi-yu43@hotmail.com>
2021-04-13 14:47:02 +0900

    初回

---
+++ Assets/Scripts/Ball.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball : MonoBehaviour
+{
+
+    public float speed = 5f;
+    // 速さの最大値を指定する変数を追加
+    public float minSpeed = 5f;
+    // 速さの最小値を指定する変数を追加
+    public float maxSpeed = 10f;
+    Rigidbody myRigidbody;
+
+    // Start is called before the first frame update
+    private void Start()
+    {
+        myRigidbody = GetComponent<Rigidbody>();
+        myRigidbody.velocity = new Vector3(speed, speed, 0f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // 現在の速度を取得
+        Vector3 velocity = myRigidbody.velocity;
+        // 速さを計算
+        float clampedSpeed = Mathf.Clamp(velocity.magnitude, minSpeed, maxSpeed);
+        // 速度を変更
+        myRigidbody.velocity = velocity.normalized * clampedSpeed;
+    }
+}
---
+++ Assets/Scripts/GameOver.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameOver : MonoBehaviour
+{
+    [SerializeField] GameObject text;
+    private void OnCollisionEnter(Collision collision)
+    {
+        Destroy(collision.gameObject);
+        text.SetActive(true);
+    }
+}
